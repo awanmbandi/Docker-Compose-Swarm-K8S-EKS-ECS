5edf1d9a04f82e084d9078302214b7f4e3c67100
awanmbandi <awanmbandi@gmail.com>
2025-02-12 17:41:07 -0600

    updated docker commands

---
+++ Compose/compose-commands.sh
@@ -0,0 +1,32 @@
+###########################################################
+################ DOCKER COMPOSE COMMANDS ##################
+###########################################################
+### Create and start containers
+docker-compose up
+
+### Stop and remove containers, networks, images, and volumes
+docker-compose down
+
+### Build or rebuild services
+docker-compose build
+
+### View output from containers
+docker-compose logs
+
+### List containers
+docker-compose ps
+
+### Restart services
+docker-compose restart
+
+### Start services
+docker-compose start
+
+### Stop services
+docker-compose stop
+
+### Pull service images
+docker-compose pull
+
+### Push service images
+docker-compose push
