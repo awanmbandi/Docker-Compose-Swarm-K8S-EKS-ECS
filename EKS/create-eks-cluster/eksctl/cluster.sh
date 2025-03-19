f1169603ddadcf24a26553f89c2090d8f2d6a4b6
awanmbandi <awanmbandi@gmail.com>
2025-03-19 11:13:18 -0500

    added eksctl create cluster command

---
+++ EKS/create-eks-cluster/eksctl/cluster.sh
@@ -0,0 +1,22 @@
+## CREATE EKS Cluster
+eksctl create cluster --name eks-cluster \
+  --version 1.30 \
+  --region us-east-2 \
+  --nodegroup-name eks-cluster-ng \
+  --node-type t2.medium \
+  --nodes 3 \
+  --nodes-min 1 \
+  --nodes-max 4 \
+  --managed
+
+## DELETE THE CLUSTER NODE GROUP
+eksctl delete nodegroup \
+  --cluster eks-cluster \
+  --name eks-cluster-ng \
+  --region us-east-2
+
+## DELETE THE EKS CLUSTER
+eksctl delete cluster \
+  --name eks-cluster \
+  --region us-east-2
+
